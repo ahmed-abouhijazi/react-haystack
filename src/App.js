e5701f70be70a575f3caecba453fb9d16c0504e1
ABOUHIJAZI <ahmed.abouhijazi@capgemini.com>
2023-04-06 23:38:52 +0000

    deploy app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import NavBar from './cmp/NavBar';
 
 function App() {
+  const [time, setTime] = useState({ minutes: 50, seconds: 0 });
+  const [intervalId, setIntervalId] = useState(null);
+
+  useEffect(() => {
+    const updateTimer = () => {
+      setTime(prevTime => {
+        const { minutes, seconds } = prevTime;
+        if (minutes === 0 && seconds === 0) {
+          clearInterval(intervalId);
+          return prevTime;
+        }
+        if (seconds === 0) {
+          return { minutes: minutes - 1, seconds: 59 };
+        }
+        return { minutes, seconds: seconds - 1 };
+      });
+    };
+    const newIntervalId = setInterval(updateTimer, 1000);
+    setIntervalId(newIntervalId);
+
+    return () => clearInterval(newIntervalId);
+  }, [intervalId]);
+
+  const { minutes, seconds } = time;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">;
+    
+      <NavBar />
+      <div class="gif-container">
+    <img src="/img/giphy.gif" alt="Your GIF" />
+    <div className="timer">{`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`}</div>
+
+  </div>
+      <div className="btn-container">
+        <button>Request</button>
+        <button>Buy</button>
+      </div>
     </div>
   );
 }
---
+++ src/cmp/NavBar.js
@@ -0,0 +1,62 @@
+import React, { useState,useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+function NavBar() {
+  const [showConnectBlock, setShowConnectBlock] = useState(false);
+  const [selectedNetwork, setSelectedNetwork] = useState('');
+
+  const handleNetworkChange = (event) => {
+    setSelectedNetwork(event.target.value);
+  };
+
+  useEffect(() => {
+    const handleClick = (e) => {
+      const connectBlock = document.querySelector('.connect-block');
+      const connectButton = document.querySelector('.connect-button');
+      if (connectBlock && !connectBlock.contains(e.target) && !connectButton.contains(e.target)) {
+        setShowConnectBlock(false);
+      }
+    };
+  
+    document.addEventListener('click', handleClick);
+  
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // TODO: handle submission of selected network
+  };
+  return (
+    <nav>
+      <img src="/img/Website_logo.png" alt="Logo" />
+      <ul>
+        <li className="element" ><a href="http://localhost:3000">Home</a></li>
+        <li className="element" ><a href="http://localhost:3000">About</a></li>
+        <li className="element" ><a href="http://localhost:3000">Contact</a></li>
+        <li className="connect">
+          <button className="connect-button" onClick={() => setShowConnectBlock(!showConnectBlock)}>Connect</button>
+          {showConnectBlock && (
+            <div className="connect-block">
+              <form onSubmit={handleSubmit}>
+                <label htmlFor="network-select">Select a network:</label>
+                <select id="network-select" value={selectedNetwork} onChange={handleNetworkChange}>
+                  <option value="">Choose a network</option>
+                  <option value="Network 1">Network 1</option>
+                  <option value="Network 2">Network 2</option>
+                  <option value="Network 3">Network 3</option>
+                </select>
+                <button type="submit">Next</button>
+              </form>
+            </div>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
