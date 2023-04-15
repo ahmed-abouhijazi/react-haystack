fe778794bbe6f59c29f8cd67b659d642b38f28d8
ABOUHIJAZI <ahmed.abouhijazi@capgemini.com>
2023-04-15 23:06:51 +0000

    deploy to ght page

--- src/cmp/NavBar.js
+++ src/cmp/NavBar.js
@@ -1,21 +1,19 @@
-import React, { useEffect } from 'react';
-// import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import './NavBar.css';
 
 function NavBar(props) {
-  // const [showConnectBlock, setShowConnectBlock] = useState(false);
-  // const [selectedNetwork, setSelectedNetwork] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // const handleNetworkChange = (event) => {
-  //   setSelectedNetwork(event.target.value);
-  // };
-
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
   useEffect(() => {
     const handleClick = (e) => {
-      const connectBlock = document.querySelector('.connect-block');
+      const connectBlock = document.querySelector('.dropdown');
       const connectButton = document.querySelector('.right-button');
       if (connectBlock && !connectBlock.contains(e.target) && !connectButton.contains(e.target)) {
-        // setShowConnectBlock(false);
+        setIsOpen(false);
       }
     };
 
@@ -25,44 +23,30 @@ function NavBar(props) {
       document.removeEventListener('click', handleClick);
     };
   }, []);
-
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   // TODO: handle submission of selected network
-  // };
   return (
     <nav>
       <img src="/react-haystack/img/Website_logo.png" alt="Logo" />
-      <ul>
+      <ul >
         <li className="element" ><a href="https://ahmed-abouhijazi.github.io/react-haystack">Home</a></li>
         <li className="element" ><a href="https://ahmed-abouhijazi.github.io/react-haystack">About</a></li>
         <li className="element" ><a href="https://ahmed-abouhijazi.github.io/react-haystack">Contact</a></li>
         <li className="connect">
           <div className='button-container'>
             <button className="left-button" onClick={() => props.setIsConnectOpen(true)}><img src='/react-haystack/img/icons8-wallet-full-of-cash-isolated-on-a-white-background-24.png' alt='User logo' /> Connect Wallet</button>
-            <button className="right-button hover-button" ><img src='/react-haystack/img/icons8-user-48.png' alt='User logo' /></button>
-            <div className="hovered-options">
-              <ul>
-                <li>Option 1</li>
-                <li>Option 2</li>
-                <li>Option 3</li>
-              </ul>
-            </div>
-            {/* {showConnectBlock && (
-            <div className="connect-block">
-              <form onSubmit={handleSubmit}>
-                <label htmlFor="network-select">Select a network:</label>
-                <select id="network-select" value={selectedNetwork} onChange={handleNetworkChange}>
-                  <option value="">Choose a network</option>
-                  <option value="Network 1">Network 1</option>
-                  <option value="Network 2">Network 2</option>
-                  <option value="Network 3">Network 3</option>
-                </select>
-                <button type="submit">Next</button>
-              </form>
-            </div>
-          )} */}
-
+            <button className="right-button button" onClick={toggleDropdown}><img src='/react-haystack/img/icons8-user-48.png' alt='User logo' /></button>
+            {isOpen && (
+              <div className="">
+                <ul className={isDarkMode ? 'dropdown darkMode' : 'dropdown'}>
+                  <li><img src="/react-haystack/img/avatar.png" alt='profile' /><strong>Profile</strong></li>
+                  <li><img className='support' src="/react-haystack/img/centre-dappel.png" alt='Support' /> <strong>Centre d'aide</strong></li>
+                  <li><img src="/react-haystack/img/parametres.png" alt='Paramètres' /><strong>Paramètres</strong></li>
+                  <li className={isDarkMode ? 'Toggle ToggleDark' : 'Toggle'}><label className="switch">
+                    <input type="checkbox" onClick={()=> setIsDarkMode(!isDarkMode)}/>
+                    <span className="slider"></span>
+                  </label></li>
+                </ul>
+              </div>
+            )}
           </div>
         </li>
       </ul>
